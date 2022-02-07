e647c8e7de9ce9a63217744349a31844be4dbe60
Berkanktk <berkank@live.dk>
2022-02-07 22:03:00 +0100

    Added selection sort

---
+++ Sorting/Selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(array):
+    for i in range(len(array)):
+        minimum = i
+        for j in range(i + 1, len(array)):
+            if array[minimum] > array[j]:
+                minimum = j
+
+        array[i], array[minimum] = array[minimum], array[i]
